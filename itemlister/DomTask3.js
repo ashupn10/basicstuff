343db3419c40e16eac3a74d5f3b2e58258d702b1
ashupn10 <ashupn10@gmail.com>
2022-11-10 21:55:13 +0530

    Done DomTask3 and answered all

---
+++ itemlister/DomTask3.js
@@ -0,0 +1,7 @@
+const header=document.getElementById('header-title');
+header.textContent="Hello";
+header.style.borderBottom= 'solid 3px #000';
+console.log('hello world');
+const itemhead= document.querySelector('.title');
+itemhead.style.fontWeight='bold';
+itemhead.style.color='Green';
